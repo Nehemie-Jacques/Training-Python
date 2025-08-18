aa597f155a61ec018b70274070c7201f3c4a26aa
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-08-18 06:38:20 +0100

    Projet4_deploiement_v2

--- Projet4_deploiement/main.py
+++ Projet4_deploiement/main.py
@@ -1,41 +1,80 @@
-import os
+# import os
+# import tarfile
+# import paramiko
+# import json
+# from scp import SCPClient
+
+# # Charger la configuration
+# with open("deploy_config.json") as f:
+#     config = json.load(f)
+
+# SERVER = config["server"]
+# USER = config["user"]
+# PASSWORD = config["password"]
+# REMOTE_PATH = config["remote_path"]
+
+# # Cration d'une archive tar.gz du projet local
+# def make_archive(source_dir, archive_name="package.tar.gz"):
+#     with tarfile.open(archive_name, "w:gz") as tar:
+#         tar.add(source_dir, arcname = os.path.basename(source_dir))
+#     print(f"Archive created: {archive_name}")
+#     return archive_name
+
+# # Envoi de l'archive sur le serveur distant
+# def deploy(archive):
+#     ssh = paramiko.SSHClient()
+#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#     ssh.connect(SERVER, username=USER, password=PASSWORD)
+
+#     with SCPClient(ssh.get_transport()) as scp:
+#         scp.put(archive, REMOTE_PATH)
+#         print(f"Archive deployed to {SERVER}:{REMOTE_PATH}")
+
+#     # Décompression de l'archive coté serveur
+#     stdin, stdout, stderr = ssh.exec_command(f"cd {REMOTE_PATH} && tar -xzf {archive}")
+#     print(stdout.read().decode(), stderr.read().decode())
+#     ssh.close()
+
+
+# if __name__ == "__main__":
+#     archive = make_archive("Projet4_deploiement")
+#     deploy(archive)
+
+
+
+
+import os 
 import tarfile
-import paramiko
 import json
-from scp import SCPClient
+import shutil
 
 # Charger la configuration
 with open("deploy_config.json") as f:
     config = json.load(f)
 
-SERVER = config["server"]
-USER = config["user"]
-PASSWORD = config["password"]
-REMOTE_PATH = config["remote_path"]
+SERVER_DIR = config["server_dir"]
+ARCHIVE_NAME = config["archive_name"]
+DEPLOY_PATH = config["deploy_path"]
 
 # Cration d'une archive tar.gz du projet local
-def make_archive(source_dir, archive_name="package.tar.gz"):
+def make_archive(source_dir, archive_name):
     with tarfile.open(archive_name, "w:gz") as tar:
         tar.add(source_dir, arcname = os.path.basename(source_dir))
-    print(f"Archive created: {archive_name}")
-    return archive_name
+    print(f"Archive créée: {archive_name}")
 
-# Envoi de l'archive sur le serveur distant
-def deploy(archive):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(SERVER, username=USER, password=PASSWORD)
+# Simulation de l'envoi de l'archive sur le serveur distant
+def transfer_archive(archive, deploy_path):
+    os.makedirs(deploy_path, exist_ok=True)
+    shutil.copy(archive, deploy_path)
+    print(f"Archive transferee vers {deploy_path}") 
 
-    with SCPClient(ssh.get_transport()) as scp:
-        scp.put(archive, REMOTE_PATH)
-        print(f"Archive deployed to {SERVER}:{REMOTE_PATH}")
-
-    # Décompression de l'archive coté serveur
-    stdin, stdout, stderr = ssh.exec_command(f"cd {REMOTE_PATH} && tar -xzf {archive}")
-    print(stdout.read().decode(), stderr.read().decode())
-    ssh.close()
-
+# Extraire l'archive sur le serveur distant
+def extract_archive(deploy_path, archive_name):
+    archive_path = os.path.join(deploy_path, archive_name)
+    with tarfile.open(archive_path, "r:gz") as tar:
+        tar.extractall(path=deploy_path)
+    print(f"Archive extrait vers {deploy_path}")
 
 if __name__ == "__main__":
-    archive = make_archive("Projet4_deploiement")
-    deploy(archive)
+    make_archive("Projet4_deploiement", ARCHIVE_NAME)
+    transfer_archive(ARCHIVE_NAME, DEPLOY_PATH)
